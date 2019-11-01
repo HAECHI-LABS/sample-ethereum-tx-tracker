8a6e57f221ec94eaafe3d8cfc01977a7f89aa382
Wonyoung Chae <skycwy223@kaist.ac.kr>
2019-11-01 13:37:33 +0900

    chore: apply updated henesis-sdk spec to sample

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const app = express();
-const Henesis = require('@haechi-labs/henesis-sdk-js').default;
+const { TransactionTracker } = require('@haechi-labs/henesis-sdk-js');
 const { Sender } = require('./helper/Sender');
 
 const transactions = {};
 
 const { CLIENT_ID, PRIVATE_KEY, NODE_ENDPOINT } = process.env;
-const henesis = new Henesis(CLIENT_ID);
+const tracker = new TransactionTracker(CLIENT_ID, {
+  platform: 'ethereum',
+  network: 'ropsten',
+});
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/api/tx', function(req,res) {
-    res.json( Object.entries(transactions).map( item => { return { ...item[1], transactionHash:item[0]} } ) ); 
+    res.json( Object.entries(transactions).map( item => { return { ...item[1], transactionHash:item[0]} } ) );
 });
 
 app.post('/api/tx', async function(req,res) {
@@ -26,12 +29,12 @@ app.post('/api/tx', async function(req,res) {
   console.log(`transaction generated. txHash:${txHash}`);
 
   //start tracking transaction
-  henesis.trackTransaction(txHash, {
-    timeout: 30*1000, 
+  tracker.trackTransaction(txHash, {
+    timeout: 30*1000,
     confirmation: 6
   });
   transactions[txHash] = { status: "registered" };
-  res.json(transactions); 
+  res.json(transactions);
 });
 
 app.get('/*', function(req, res) {
@@ -39,24 +42,25 @@ app.get('/*', function(req, res) {
 });
 
 async function trackTx () {
-  const subscription = await henesis.subscribe(
+  const subscription = await tracker.subscribe(
     "transaction",
-    {
-      subscriptionId: "your-subscription-id"
-    }
+      {
+        subscriptionId: "your-subscription-id",
+        ackTimeout: 30 * 1000 // default is 10 * 1000 (ms)
+      }
   );
 
   subscription.on("message", async (message) => {
     console.log(`now transaction status is: ${message.data.type}`)
     switch(message.data.type) {
-      case 'pending' : 
+      case 'pending' :
         transactions[message.data.result.transactionHash] = { status: 'pending'}
         break;
-      case 'receipt' : 
+      case 'receipt' :
         console.log('message.data.result',message.data.result)
         transactions[message.data.result.transactionHash] = { ...message.data.result, status: 'receipt' }
         break;
-      case 'confirmation' : 
+      case 'confirmation' :
         console.log('message.data.result',message.data.result)
         transactions[message.data.result.transactionHash] = {...message.data.result, status: 'confirmation' }
         break;
@@ -65,13 +69,13 @@ async function trackTx () {
   });
 
   subscription.on("error", async (error) => {
-    console.log('err',error)
+    console.log('err',error);
   });
 }
 
 async function main () {
-    trackTx()
+    trackTx();
     app.listen(3000);
 }
 
-main()
+main();
